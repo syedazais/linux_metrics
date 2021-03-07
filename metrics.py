0588eda5f02c342544f8c39a98a865ce7a9af058
syedazais <43855972+syedazais@users.noreply.github.com>
2021-03-07 21:54:11 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,5 +1,4 @@
 import psutil
 print('******************cpu usage:***************** \n',psutil.cpu_times())
-#print('**********************memory full info:************** \n', psutil.memory_full_info())
 print('*********swap memory:**************** \n', psutil.swap_memory())
 print('*********virtual memory :********* ', psutil.virtual_memory()._asdict())
